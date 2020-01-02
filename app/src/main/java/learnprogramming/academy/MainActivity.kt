13d93fddf2ef826370639d02c2d6d84ff4c8a1c7
Bogdan <bogdan.oloeriu@gmail.com>
2020-01-02 22:48:06 +0100

    we have portret buttons

--- app/src/main/java/learnprogramming/academy/MainActivity.kt
+++ app/src/main/java/learnprogramming/academy/MainActivity.kt
@@ -9,7 +9,6 @@ import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 private const val TAG = "MyMainActivity"
-private const val TEXT_CONTENTS = "TextContent"
 
 class MainActivity : AppCompatActivity() {
 
@@ -17,66 +16,9 @@ class MainActivity : AppCompatActivity() {
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        Log.d(TAG, "onCreate: ")
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val userInput: EditText = findViewById(R.id.editText)
-        val button: Button = findViewById(R.id.button)
-        textView = findViewById(R.id.textView)
-        textView?.movementMethod = ScrollingMovementMethod()
-        textView?.text = ""
-        userInput.setText("")
-
-        button.setOnClickListener {
-            Log.d(TAG, "onClick: ")
-            textView?.append(userInput.text.toString() + "\n")
-            userInput.setText("")
-        }
     }
-
-    override fun onStart() {
-        Log.d(TAG, "onStart: ")
-        super.onStart()
-    }
-
-    override fun onSaveInstanceState(outState: Bundle?) {
-        Log.d(TAG, "onSaveInstanceState: ")
-        super.onSaveInstanceState(outState)
-        outState?.putString(TEXT_CONTENTS, textView?.text.toString())
-    }
-
-    override fun onRestoreInstanceState(savedInstanceState: Bundle?) {
-        Log.d(TAG, "onRestoreInstanceState: ")
-        super.onRestoreInstanceState(savedInstanceState)
-
-        textView?.text = savedInstanceState?.getString(TEXT_CONTENTS, "")
-    }
-
-    override fun onResume() {
-        Log.d(TAG, "onResume: ")
-        super.onResume()
-    }
-
-    override fun onPause() {
-        Log.d(TAG, "onPause: ")
-        super.onPause()
-    }
-
-    override fun onStop() {
-        Log.d(TAG, "onStop: ")
-        super.onStop()
-    }
-
-    override fun onRestart() {
-        Log.d(TAG, "onRestart: ")
-        super.onRestart()
-    }
-
-    override fun onDestroy() {
-        Log.d(TAG, "onDestroy: ")
-        super.onDestroy()
-    }
-
 
 }
