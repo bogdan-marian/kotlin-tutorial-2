c23fca69a29b9307e44dc5ec0fa29933dd8da56b
Bogdan Oloeriu <bogdan.oloeriu@gmail.com>
2020-01-05 13:05:40 +0100

    endless loop

--- app/src/main/java/learnprogramming/academy/MainActivity.kt
+++ app/src/main/java/learnprogramming/academy/MainActivity.kt
@@ -1,10 +1,7 @@
 package learnprogramming.academy
 
 import android.os.Bundle
-import android.view.View
-import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import kotlinx.android.synthetic.main.activity_main.*
 
 //private const val TAG = "MyMainActivity"
 
@@ -13,6 +10,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        while (true) {
+            val x = 1
+        }
 
     }
 }
