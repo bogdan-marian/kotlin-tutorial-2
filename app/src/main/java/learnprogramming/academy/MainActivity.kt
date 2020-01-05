d9e97759ecbb8357c456e9de547bbedc47781248
Bogdan Oloeriu <bogdan.oloeriu@gmail.com>
2020-01-05 13:20:14 +0100

    added tags

--- app/src/main/java/learnprogramming/academy/MainActivity.kt
+++ app/src/main/java/learnprogramming/academy/MainActivity.kt
@@ -1,18 +1,34 @@
 package learnprogramming.academy
 
+import android.os.AsyncTask
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 
 //private const val TAG = "MyMainActivity"
 
 class MainActivity : AppCompatActivity() {
 
+    private val TAG = "MainActivity"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        while (true) {
-            val x = 1
+        Log.d(TAG, "onCreate called")
+
+    }
+
+    private inner class DownloadData : AsyncTask<String, Void, String>() {
+
+        private val TAG = "DownloadData"
+
+        override fun onPostExecute(result: String?) {
+            super.onPostExecute(result)
+        }
+
+        override fun doInBackground(vararg p0: String?): String {
+            return ""
         }
 
     }
