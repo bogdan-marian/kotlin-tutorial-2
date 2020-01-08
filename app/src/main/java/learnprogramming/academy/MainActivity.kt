fcf03e630b527b7718d33e9b4981a515764832c8
Bogdan <bogdan.oloeriu@gmail.com>
2020-01-08 23:47:01 +0100

    deleted and recreated main activity, added menu by hand

--- app/src/main/java/learnprogramming/academy/MainActivity.kt
+++ app/src/main/java/learnprogramming/academy/MainActivity.kt
@@ -1,18 +1,31 @@
 package learnprogramming.academy
 
 import android.os.Bundle
-import android.view.View
-import android.widget.Button
+import android.util.Log
+import android.view.Menu
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
-//private const val TAG = "MyMainActivity"
+private val TAG = "MainActivity"
 
 class MainActivity : AppCompatActivity() {
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        Log.d(TAG, "onCreate called")
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
 
+        Log.d(TAG, "onCreate ends")
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        Log.d(TAG, "onCreateOptionsMenu called")
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+
 }
