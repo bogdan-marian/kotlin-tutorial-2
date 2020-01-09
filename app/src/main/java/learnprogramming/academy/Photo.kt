7b8b6a5c71eb9533fce8a6d015a78b4ed97f83a0
Bogdan <bogdan.oloeriu@gmail.com>
2020-01-09 22:36:05 +0100

    parsing json data

---
+++ app/src/main/java/learnprogramming/academy/Photo.kt
@@ -0,0 +1,19 @@
+package learnprogramming.academy
+
+class Photo(
+    val title: String,
+    val author: String,
+    val authorId: String,
+    val link: String,
+    val tags: String,
+    val image: String
+) {
+    override fun toString(): String {
+        return "Photo(title='$title', " +
+                "author='$author', " +
+                "authorId='$authorId', " +
+                "link='$link', " +
+                "tags='$tags', " +
+                "image='$image')"
+    }
+}
