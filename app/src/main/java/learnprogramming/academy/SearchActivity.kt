19dfab96a1c1e81e5c45d24f2d677d27f419046c
Bogdan Oloeriu <bogdan.oloeriu@gmail.com>
2020-01-12 10:54:27 +0100

    145 nexted layouts

--- app/src/main/java/learnprogramming/academy/SearchActivity.kt
+++ app/src/main/java/learnprogramming/academy/SearchActivity.kt
@@ -12,10 +12,7 @@ class SearchActivity : AppCompatActivity() {
         setContentView(R.layout.activity_search)
         setSupportActionBar(toolbar)
 
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
-        }
+
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
     }
 
